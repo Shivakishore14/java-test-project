c93e19c4477588319231b3e3048e8f326a2f69fd
shivakishore14 <shivakishore14@gmail.com>
2018-05-22 14:15:17 +0530

    changes to code as per review #JTP-CR-1

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,10 +1,10 @@
 public class Sample {
-    public void add(int a, int b){
-        System.out.println("result = " + a+b);
+    public int add(int a, int b){
+        return a+b;
     }
     public static void main(String[] args) {
         System.out.println("This is a sample class");
         Sample s = new Sample();
-        s.add(2,3);
+        System.out.println( s.add(2,3) );
     }
 }
