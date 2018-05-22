a959f68b67c24c5f7ee005cf20964c6174174a87
shivakishore14 <shivakishore14@gmail.com>
2018-05-22 15:29:59 +0530

    added new feature

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -2,12 +2,16 @@ public class Sample {
     public void sample_fun(){
         // TODO : complete this
     }
+    public void newFeature() {
+        System.out.println("New feature implemented");
+    }
     public int add(int a, int b){
         return a+b;
     }
     public static void main(String[] args) {
         System.out.println("This is a sample class");
         Sample s = new Sample();
+        s.newFeature();
         System.out.println( s.add(2,3) );
     }
 }
